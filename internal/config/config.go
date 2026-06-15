08e8b32b8a4f37f84641594d4485266dfbd1a890
Eng-Moaz <156330236+Eng-Moaz@users.noreply.github.com>
2026-06-15 06:15:21 +0300

    addded config functionalities

---
+++ internal/config/config.go
@@ -0,0 +1,69 @@
+package config 
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+type Config struct{
+	DBURL string `json:"db_url"`
+	USERNAME string `json:"current_user_name"`
+}
+
+func getConfigPath() (string, error){
+	homeDir, err := os.UserHomeDir()
+	if err != nil{
+		return "", err
+	}
+	jsonDir := filepath.Join(homeDir, ".gatorconfig.json")
+	return jsonDir, nil
+}
+
+func Read() (Config, error) {
+	jsonDir, err := getConfigPath()
+	if err != nil{
+		return Config{}, err
+	}
+
+	var config Config
+	file, err := os.Open(jsonDir)
+	if err != nil{
+		return Config{}, err
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil{
+		return Config{}, err
+	}
+	return config, nil
+}
+
+func (c *Config) SetUser(username string){
+	c.USERNAME = username
+	write(*c)
+}
+
+
+func write(cfg Config) error{
+	jsonDir, err := getConfigPath()
+	if err != nil{
+		return err
+	}
+	file, err := os.Create(jsonDir)	
+	if err != nil{
+		return err
+	}
+	defer file.Close()
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(cfg)
+	if err != nil{
+		return err
+	}
+	return nil
+}
+
+
+
+
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"fmt"
+	"github.com/Eng-Moaz/RSSAggregator/interal/config"
+)
+
+func main(){
+	cfg, err := config.Read()
+	if err != nil{
+		log.Fatal("Couldn't read the json file")
+	}
+	cfg.SetUser("Moaz")
+	cfg, err = config.Read()
+	if err != nil{
+		log.Fatal("Couldn't read the json file")
+	}
+
+	fmt.Println(cfg)
+}
